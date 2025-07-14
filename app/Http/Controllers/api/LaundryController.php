dae5f45ee5b19321ead0d07cfb8eb98c2b43d22d
ryhndastra <ryhndastra@gmail.com>
2025-07-14 12:11:54 +0700

    add claim laundry and user order

--- app/Http/Controllers/api/LaundryController.php
+++ app/Http/Controllers/api/LaundryController.php
@@ -14,4 +14,53 @@ class LaundryController extends Controller
             'data' => $laundries
         ],200);
     }
+
+    public function whereUserId($id){
+        $laundries = Laundry::where('user_id','=', $id)->with('user','shop')->orderBy('created_at', 'desc')->get();
+
+        if(count($laundries) > 0){
+            return response()->json([
+            'data' => $laundries
+            ],200);
+        }else{
+            return response()->json([
+            'data' => [],
+            'message' => 'Data tidak ditemukan'
+            ],404);
+        }
+    }
+
+    public function claimLaundry(Request $request){
+        $laundry = Laundry::where([
+            ['id','=',$request->id],
+            ['claim_code','=',$request->claim_code],
+        ])->first();
+
+        if(!$laundry){
+            return response()->json([
+            'data' => [],
+            'message' => 'Data tidak ditemukan'
+            ],404);
+        }
+
+        if($laundry->user_id != 0){
+            return response()->json([
+            'message' => 'Laundry sudah diambil'
+            ],400);
+        }
+
+        $laundry->user_id = $request->user_id;
+        $updated = $laundry->save();
+
+        if($updated){
+            return response()->json([
+            'message' => 'Laundry berhasil diambil',
+            'data' => $updated
+            ],201);
+        }else{
+            return response()->json([
+            'message' => 'Laundry gagal diambil'
+            ],400);
+        }
+    }
 }
